d7591d45627796134725bc45a4b7aaf3bde0a53f
Edward <edward.azarevich@gmail.com>
2026-09-17 21:46:34 +0300

    review types

--- main.go
+++ main.go
@@ -6,8 +6,7 @@ import (
 )
 
 func main() {
-	var userHeight float64 = 1.9
-	var userkg float64 = 89
-	var IMT = userkg / math.Pow(userHeight, 2)
+	userHeight, userkg := 1.9, 89.0
+	IMT := userkg / math.Pow(userHeight, 2)
 	fmt.Printf("Halo, your IMT is %.2f\n", IMT)
 }

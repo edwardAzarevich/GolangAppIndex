801a37a2cd99250400793486d27f8c2ac7c8e3fc
Edward <edward.azarevich@gmail.com>
2026-09-23 20:02:44 +0300

    add new decomposition code ( new func output result)

--- 2-imt/main.go
+++ 2-imt/main.go
@@ -5,14 +5,20 @@ import (
 	"math"
 )
 
+func outputResult(imt float64) string {
+	result := fmt.Sprintf("Your index is: %.0f", imt)
+	fmt.Println(result)
+}
+
 func main() {
 	const IMTPower = 2
 	var userHeight float64
 	var userKg float64
-	fmt.Print("Enter your Hieght in meters ")
+	fmt.Println("calc index")
+	fmt.Print("Enter your Hieght in santimeters ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Enter your Kg ")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight, IMTPower)
-	fmt.Print(IMT)
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	outputResult(IMT)
 }

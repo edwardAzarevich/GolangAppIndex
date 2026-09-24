9a49155362c4b54f79f7c1ed1ae41b515439079c
Edward <edward.azarevich@gmail.com>
2026-09-24 17:22:34 +0300

    add new functions

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func userInput() float64 {
+	var userInput float64
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculateConversion(userInput float64, current, target float64) float64 {
+	return userInput * (target / current)
+}
+
 func main() {
 	const (
 		usdToEur = 0.92
